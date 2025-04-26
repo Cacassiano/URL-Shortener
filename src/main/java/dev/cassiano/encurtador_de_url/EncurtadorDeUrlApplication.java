a404515e0d72afe05b6863fc777fa3a5a567dc77
Cassiano <saurinho.64.ss@gmail.com>
2025-04-26 12:41:05 -0300

    First commit

---
+++ src/main/java/dev/cassiano/encurtador_de_url/EncurtadorDeUrlApplication.java
@@ -0,0 +1,13 @@
+package dev.cassiano.encurtador_de_url;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncurtadorDeUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncurtadorDeUrlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/cassiano/encurtador_de_url/EncurtadorDeUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cassiano.encurtador_de_url;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncurtadorDeUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
